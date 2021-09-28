fc5f3cfc11e5b261bcccb07e12ec1f2e0791f614
Riki420 <girottiriccardo1998@gmail.com>
2021-09-28 15:37:38 +0200

    Select -ES3

--- university.sql
+++ university.sql
@@ -4,3 +4,6 @@ SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
 
 /*Selezionare tutti i corsi che valgono più di 10 crediti*/
 SELECT * FROM `courses` WHERE `cfu` > 10;
+
+/*Selezionare tutti gli studenti che hanno più di 30 anni*/
+SELECT `name`, `surname`,`date_of_birth` FROM `students` WHERE `date_of_birth` <= '1991-09-28'
