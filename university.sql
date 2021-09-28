f9f09de94ff065edc1ad7eae7ec2df664fa054f9
Riki420 <girottiriccardo1998@gmail.com>
2021-09-28 15:29:34 +0200

    Select - ES2

--- university.sql
+++ university.sql
@@ -2,3 +2,5 @@
 /*Selezionare tutti gli studenti nati nel 1990*/
 SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
 
+/*Selezionare tutti i corsi che valgono più di 10 crediti*/
+SELECT * FROM `courses` WHERE `cfu` > 10;
