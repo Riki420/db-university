12244aafe4cfbf7d1710b967333adfc58949f4ec
Riki420 <girottiriccardo1998@gmail.com>
2021-09-28 15:57:17 +0200

    Select - ES4

--- university.sql
+++ university.sql
@@ -7,3 +7,6 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 
 /*Selezionare tutti gli studenti che hanno più di 30 anni*/
 SELECT `name`, `surname`,`date_of_birth` FROM `students` WHERE `date_of_birth` <= '1991-09-28'
+
+/*Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea*/
+SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1
