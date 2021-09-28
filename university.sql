69705c822b75475c89e4892c05fb837eed208b6e
Riki420 <girottiriccardo1998@gmail.com>
2021-09-28 16:03:42 +0200

    Select - ES6

--- university.sql
+++ university.sql
@@ -15,3 +15,4 @@ SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1;
 SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` >= '14:00:00';
 
 /*Selezionare tutti i corsi di laurea magistrale*/
+SELECT * FROM `degrees` WHERE `level` = 'magistrale';
