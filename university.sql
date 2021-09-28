8f3959251837038a43ca9a92c31c40f841048660
Riki420 <girottiriccardo1998@gmail.com>
2021-09-28 16:02:12 +0200

    Select - ES5

--- university.sql
+++ university.sql
@@ -6,7 +6,12 @@ SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
 SELECT * FROM `courses` WHERE `cfu` > 10;
 
 /*Selezionare tutti gli studenti che hanno più di 30 anni*/
-SELECT `name`, `surname`,`date_of_birth` FROM `students` WHERE `date_of_birth` <= '1991-09-28'
+SELECT `name`, `surname`,`date_of_birth` FROM `students` WHERE `date_of_birth` <= '1991-09-28';
 
 /*Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea*/
-SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1
+SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1;
+
+/*Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020*/
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` >= '14:00:00';
+
+/*Selezionare tutti i corsi di laurea magistrale*/
